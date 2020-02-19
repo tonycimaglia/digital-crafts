137294c912c4e146137b5e8995c30e89fc6dc411
tonycimaglia <tonycimaglia@gmail.com>
2020-02-18 21:17:49 -0500

    wrote function to get city name from text input.

--- weather.js
+++ weather.js
@@ -3,9 +3,14 @@
 const searchButton = document.querySelector('button');
 // register click event
 searchButton.addEventListener('click', function() {
-  console.log('click event registered');
+  console.log(getCityName());
 });
 
 // take data from city input
+function getCityName() {
+  const cityInput = document.querySelector('input');
+  return cityInput.value;
+}
 // make api call to api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+
 // make second call for weather icon to: http://openweathermap.org/img/wn/10d@2x.png
