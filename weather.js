1ab4cdfef1e140db42de3c50af08f5361106809e
tonycimaglia <tonycimaglia@gmail.com>
2020-02-18 21:10:01 -0500

    selected button in js and registered click event for button variable.

--- weather.js
+++ weather.js
@@ -1,1 +1,11 @@
-console.log(`loaded`);
+
+// select button
+const searchButton = document.querySelector('button');
+// register click event
+searchButton.addEventListener('click', function() {
+  console.log('click event registered');
+});
+
+// take data from city input
+// make api call to api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+// make second call for weather icon to: http://openweathermap.org/img/wn/10d@2x.png
