aecd82b1e0735ea752e085e8f61cf88da2f9e341
tonycimaglia <32717480+tonycimaglia@users.noreply.github.com>
2020-02-20 11:02:19 -0500

    wrote a function to retrieve data from open weather api using text from city input and api key

--- weather.js
+++ weather.js
@@ -4,6 +4,8 @@ const searchButton = document.querySelector('button');
 // register click event
 searchButton.addEventListener('click', function () {
   const cityName = getCityName();
+  console.log(cityName);
+  getWeatherData(cityName);
   // search api for city name
 });
 
@@ -12,7 +14,22 @@ function getCityName() {
   const cityInput = document.querySelector('input');
   return cityInput.value;
 }
-// make api call to api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
-// grab api key from env variable
-const apiKey = 'fakeKeyHere';
+
+
+  // make api call to api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+function getWeatherData( cityName ) {
+  // eventually, grab api key from env variable
+  var key = 'fakekeyhere';
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`)
+    .then((response) => {
+    return response.json();
+  })
+    .then((myJson) => {
+    console.log(myJson);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
 // make second call for weather icon to: http://openweathermap.org/img/wn/10d@2x.png
