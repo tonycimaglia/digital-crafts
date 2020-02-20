41687e11c068cbab15b20f4daf3aab8c69de886a
tonycimaglia <tonycimaglia@gmail.com>
2020-02-20 17:03:12 -0500

    wrote function to fetch image for current weather with data from initial response.

--- weather.js
+++ weather.js
@@ -1,12 +1,13 @@
 
 // select button
 const searchButton = document.querySelector('button');
+
 // register click event
 searchButton.addEventListener('click', function () {
   const cityName = getCityName();
-  console.log(cityName);
-  getWeatherData(cityName);
   // search api for city name
+  getWeatherData(cityName);
+  // apend weather data
 });
 
 // take data from city input
@@ -15,21 +16,31 @@ function getCityName() {
   return cityInput.value;
 }
 
-
-  // make api call to api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
-function getWeatherData( cityName ) {
+// make api call to openweather api
+function getWeatherData(cityName) {
   // eventually, grab api key from env variable
-  var key = 'fakekeyhere';
+  const key = 'your key here';
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`)
     .then((response) => {
-    return response.json();
-  })
-    .then((myJson) => {
-    console.log(myJson);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+      return response.json();
+    })
+    .then((weatherData) => {
+      //make 2nd api call
+      getWeatherImage(weatherData.weather[0].icon)
+      return weatherData;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 }
 
-// make second call for weather icon to: http://openweathermap.org/img/wn/10d@2x.png
+// make second call for weather icon to api
+function getWeatherImage(iconValue) {
+  fetch(`http://openweathermap.org/img/wn/${iconValue}@2x.png`)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
