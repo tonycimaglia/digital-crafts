3506a8510aa304f5112140d48f261bde2048c4ae
tonycimaglia <tonycimaglia@gmail.com>
2020-02-19 20:59:40 -0500

    Stored city name to a variable.

--- weather.js
+++ weather.js
@@ -2,8 +2,9 @@
 // select button
 const searchButton = document.querySelector('button');
 // register click event
-searchButton.addEventListener('click', function() {
-  console.log(getCityName());
+searchButton.addEventListener('click', function () {
+  const cityName = getCityName();
+  // search api for city name
 });
 
 // take data from city input
@@ -12,5 +13,6 @@ function getCityName() {
   return cityInput.value;
 }
 // make api call to api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
-
+// grab api key from env variable
+const apiKey = 'fakeKeyHere';
 // make second call for weather icon to: http://openweathermap.org/img/wn/10d@2x.png
